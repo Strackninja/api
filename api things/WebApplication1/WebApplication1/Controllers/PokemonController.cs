a7b80982a88b55ad29cb367d52454d4e274f48d7
Strackninja <strahinja.velickovic018@gmail.com>
2020-02-19 14:55:03 +0100

    02-19

---
+++ api things/WebApplication1/WebApplication1/Controllers/PokemonController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/pokemon")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+
+        public static List<Pokemon> pokemons = new List<Pokemon>()
+        {
+            new Pokemon() {Name = "Ekans", Strength = 67},
+            new Pokemon() {Name = "Bulbasaur", Strength = 200},
+            new Pokemon() {Name = "arts", Strength = 9874},
+        };
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            //Pokemon p = new Pokemon();
+            //p.Name = "Ekans";
+            //p.Strength = 67;
+
+            return Ok(pokemons);
+        }
+
+
+
+    }
+}
---
+++ api things/WebApplication1/WebApplication1/Pokemon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class Pokemon
+    {
+        public string Name
+        {
+            get
+            {
+                return Name;
+            }
+
+            set
+            {
+                Name = value.ToUpper();
+            }
+        }
+
+        public int Strength { get; set; }
+    }
+}
